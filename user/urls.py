92fca5391ef08ddebc75cd13577e8c351c1185b3
Haseeb Majid <haseeb.majid@spyresync.com>
2022-07-01 15:29:57 +0500

    changes

--- user/urls.py
+++ user/urls.py
@@ -6,18 +6,18 @@ from .views import AdminLoginView, UserChangePasswrodView, SendPasswordResetEmai
 
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
-router.register('user-profile-details-view', views.UserProfileDetailsView, basename='user_profile_details_view')
-router.register('user-profile-status-update-view', views.UserProfileStatusUpdateView,
+router.register('user_profile_details_view', views.UserProfileDetailsView, basename='user_profile_details_view')
+router.register('user_profile_status_update_view', views.UserProfileStatusUpdateView,
                 basename='user_profile_status_update_view')
 router.register('user-collection', views.UserCollection, basename='user_collection')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('profile/', UserProfileListView.as_view({'get': 'get'}), name=""),
-    path('profile-update/', UserProfileUpdateView.as_view({'patch': 'patch', 'post': 'post'}), name=""),
-    path('admin-login/', AdminLoginView.as_view(), name="login"),
-    path('change-password/', UserChangePasswrodView.as_view(), name="change_password"),
-    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name="send_reset_password_email"),
-    path('rest-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='rest_password'),
-    path('user-status/<int:pk>/', views.UserStatusView.as_view(), name="user_status"),
+    path('profile_update/', UserProfileUpdateView.as_view({'patch': 'patch', 'post': 'post'}), name=""),
+    path('admin_login/', AdminLoginView.as_view(), name="login"),
+    path('change_password/', UserChangePasswrodView.as_view(), name="change_password"),
+    path('send_reset_password-email/', SendPasswordResetEmailView.as_view(), name="send_reset_password_email"),
+    path('rest_password/<uid>/<token>/', UserPasswordResetView.as_view(), name='rest_password'),
+    path('user_status/<int:pk>/', views.UserStatusView.as_view(), name="user_status"),
 ]
