578d1ea1b7b26fd1af5f3de2e13f24e33eec2d92
SpyreSync <faisal>
2022-07-04 16:19:58 +0500

    mearge authentication and user_managment_again

--- user/serializers.py
+++ user/serializers.py
@@ -6,10 +6,10 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from rest_framework import serializers
 from user.models import User
-from blockchain.models import Collection
+from blockchain.models import Collection, UserWalletAddress
 
 
-class UserLoginSerializer(serializers.ModelSerializer):
+class AdminLoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255)
 
     class Meta:
@@ -18,7 +18,7 @@ class UserLoginSerializer(serializers.ModelSerializer):
 
 
 
-class UserChangePasswordSerializer(serializers.Serializer):
+class AdminChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(max_length=255, style={'input_type': 'password'}, write_only=True)
     password1 = serializers.CharField(max_length=255, style={'input_type': 'password'}, write_only=True)
     password2 = serializers.CharField(max_length=255, style={'input_type': 'password'}, write_only=True)
@@ -94,28 +94,30 @@ class UserPassowrdResetSerializer(serializers.Serializer):
 
 class UserProfileSerializer(serializers.ModelSerializer):
     # metamaskAddress = UserWalletAddressSerializer(many=False)
-    metamask = serializers.SerializerMethodField('get_metamask')
+    # metamask = serializers.SerializerMethodField('get_metamask')
 
     class Meta:
         model = User
-        fields = ["id", "profile_picture", "cover_picture", "name", "username", "email", "facebook_link", "twitter_link",
-                  "discord_link", "instagram_link", "reddit_link", "metamask"]
+        fields = ["id", "profile_picture", "cover_picture", "name", "username", "email", "facebook_link",
+                  "twitter_link", "discord_link", "instagram_link", "reddit_link"]
         # fields = "__all__"
 
-    def get_metamask(self, obj):
-        try:
-            return obj.metamaskAddress.walletAddress
-        except:
-            return None
+    # def get_metamask(self, obj):
+    #     try:
+    #         return obj.metamaskAddress.walletAddress
+    #     except:
+    #         return None
 
 
-class UserStatusViewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "status"]
+# class UserStatusViewSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ["id", "status"]
+
+# class UserWalletAddressSerializer(serializers.ModelSerializer):
 
 
-class UserProfileStatusViewSerializer(serializers.ModelSerializer):
+class UserProfileDetailsViewSerializer(serializers.ModelSerializer):
     wallet_addres = serializers.SerializerMethodField('get_metamask')
 
     class Meta:
@@ -124,7 +126,9 @@ class UserProfileStatusViewSerializer(serializers.ModelSerializer):
 
     def get_metamask(self, obj):
         try:
-            return obj.metamask_address.wallet_address
+            wallet = UserWalletAddress.objects.filter(user_wallet__id=obj.id).first()
+            if wallet:
+                return wallet.wallet_address
         except:
             return None
 
--- user/urls.py
+++ user/urls.py
@@ -1,23 +1,24 @@
 from django.urls import path, include
 
 from . import views
-from .views import AdminLoginView, UserChangePasswrodView, SendPasswordResetEmailView, UserPasswordResetView, \
-    UserProfileListView, UserProfileUpdateView
+from .views import AdminLoginView, AdminChangePasswrodView, SendPasswordResetEmailView, UserPasswordResetView, \
+    UserProfileListView, UserProfileCreateView, UserProfileUpdateView
 
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('user_profile_details_view', views.UserProfileDetailsView, basename='user_profile_details_view')
 router.register('user_profile_status_update_view', views.UserProfileStatusUpdateView,
                 basename='user_profile_status_update_view')
-router.register('user-collection', views.UserCollection, basename='user_collection')
+router.register('user_collection', views.UserCollection, basename='user_collection')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('profile/', UserProfileListView.as_view({'get': 'get'}), name=""),
-    path('profile_update/', UserProfileUpdateView.as_view({'patch': 'patch', 'post': 'post'}), name=""),
+    path('profile/', UserProfileListView.as_view({'get': 'get'}), name="profile"),
+    path('profile_create/', UserProfileCreateView.as_view({'post': 'post'}), name="profile_create"),
+    path('profile_update/<int:pk>/', UserProfileUpdateView.as_view({'patch': 'patch'}), name="profile_update"),
     path('admin_login/', AdminLoginView.as_view(), name="login"),
-    path('change_password/', UserChangePasswrodView.as_view(), name="change_password"),
-    path('send_reset_password-email/', SendPasswordResetEmailView.as_view(), name="send_reset_password_email"),
+    path('change_password/', AdminChangePasswrodView.as_view(), name="change_password"),
+    path('send_reset_password_email/', SendPasswordResetEmailView.as_view(), name="send_reset_password_email"),
     path('rest_password/<uid>/<token>/', UserPasswordResetView.as_view(), name='rest_password'),
-    path('user_status/<int:pk>/', views.UserStatusView.as_view(), name="user_status"),
+    # path('user_status/', views.UserStatusView.as_view(), name="user_status"),
 ]
