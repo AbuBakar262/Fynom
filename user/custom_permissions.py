f44003a2319fe241305577f0ea7e08330a2556f0
SpyreSync <faisal>
2022-07-19 17:06:48 +0500

    fix permission

--- user/custom_permissions.py
+++ user/custom_permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 class IsApprovedUser(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.user.status == 'Approve':
+        if request.user.status == 'Approved':
             return True
         return False
 
