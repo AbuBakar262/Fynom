d4d5ccdee0a4e027ce2bcf60a03a65bc3392a3c1
SpyreSync <faisal>
2022-08-22 14:57:22 +0500

    set_cron_job

--- blockchain/cron.py
+++ blockchain/cron.py
@@ -2,6 +2,8 @@ import logging
 import os
 from rest_framework.response import Response
 from rest_framework import status
+import calendar
+import datetime
 from blockchain.models import *
 from user.models import User
 from user.utils import Utill
@@ -14,43 +16,51 @@ logger = logging.getLogger(__name__)
 def SendEmailToWinner():
     """send email to the action winner user, to claim nft"""
 
-    # try:
-    logger.info("good 1")
-    user_wallet_no = 'newuser'
-    nft_token_id = '123456789'
-    price = '0.35895'
-    logger.info("good 2")
-    user = User.objects.all()
-    wallet_info = UserWalletAddress.objects.filter(wallet_address=user_wallet_no).first()
-    logger.info("good 3")
-    user = User.objects.filter(id=wallet_info.user_wallet.id).first()
-    logger.info("good 4")
-    nft = NFT.objects.filter(token_id=nft_token_id).first() #nft.nft_title
-    logger.info("good 5")
+    try:
+        logger.info("--------------API Called-------------")
+        date = datetime.datetime.utcnow()
+        utc_time = calendar.timegm(date.utctimetuple())
+        nfts = NFT.objects.filter(nft_sell_type="Timed Auction", end_datetime__lt=utc_time, is_listed=True)
+        if nfts:
+            for nft in nfts:
+                highest_bid = BidOnNFT.objects.filter(nft_detail=nft.id).order_by('id').first()
+                user = User.objects.filter(id=highest_bid.bidder_profile.id).first()
 
-    if user.email:
-        body = f"You are the winner of '{nft.nft_title}' NFT. You win this NFT by bidding of {price} ETH. " \
-               f"To visit and claim your " \
-               f"NFT click on the given link " + os.getenv('FRONTEND_SHOW_NFT_URL') + str(nft.id)
-        logger.info("good 6")
-        data = {
-            'subject': 'Claim Your Phynom NFT',
-            'body': body,
-            'to_email': user.email
-        }
-        logger.info("good 7")
-        Utill.send_email(data)
-        logger.info("good 8")
-        return Response({
-            "status": True, "status_code": 200, 'msg': 'Email sent to the user for claim NFT.',
-            "data": []}, status=status.HTTP_200_OK)
-    logger.info("good 9")
-    return Response({
-        "status": True, "status_code": 200, 'msg': 'Your email not found.',
-        "data": []}, status=status.HTTP_200_OK)
+                if nft.user != user:
+                    if user.email:
+                        body = f"You are the winner of '{nft.nft_title}' NFT. You win this NFT by bidding of {highest_bid.bid_price} ETH. " \
+                               f"To visit and claim your " \
+                               f"NFT click on the given link " + os.getenv('FRONTEND_SHOW_NFT_URL') + str(nft.id)
 
+                        data = {
+                            'subject': 'Claim Your Phynom NFT',
+                            'body': body,
+                            'to_email': user.email
+                        }
 
-    # except Exception as e:
-    #     return Response({
-    #         "status": False, "status_code": 400, 'msg': e.args[0],
-    #         "data": []}, status=status.HTTP_400_BAD_REQUEST)
+                        Utill.send_email(data)
+                        nft.is_listed = False
+                        nft.save()
+                        logger.info("Email send to user for clime NFT")
+
+                elif nft.user == user:
+                    body = f"On your Listed '{nft.nft_title}' NFT. No bid placed by anyone. " \
+                           f"Visit your NFT and List it again by " \
+                           f"click on the given link " + os.getenv('FRONTEND_SHOW_NFT_URL') + str(nft.id)
+
+                    data = {
+                        'subject': 'Bid Status of Your Phynom NFT',
+                        'body': body,
+                        'to_email': user.email
+                    }
+
+                    Utill.send_email(data)
+                    nft.is_listed = False
+                    nft.save()
+                    logger.info("Email send to owner no bid placed on this NFT.")
+        else:
+            logger.info("No NFT is waiting for claim.")
+
+    except Exception as e:
+        logger.info(e.args[0])
+
